28e400dbd5397a966a1a4eb53723c0e58ac660f1
Viktor Rura <ruravictor@gmail.com>
2024-07-14 13:54:57 +0100

    Working on fetching sata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-          <h1>My navbar</h1>
-        </nav>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
-import type { Metadata } from 'next'
-
- 
-export const metadata: Metadata = {
-  title: "Title",
-  description: "Description",
-}
 
 export default function Home() {
   return (
-    <main className="min-h-full grid place-content-center bg-slate-500">
-      <h1 className="text-3xl text-indigo-800">Hello world</h1>
-      <Link href="/about">
-        Go to about page
-      </Link>
+    <main>
+      <h1>Home page</h1>
+      <p>
+        <Link href="/users">Users</Link>
+      </p>
     </main>
   );
 }

106656fa609999ec74dcbc86faaacde9c4af3d7a
Viktor Rura <ruravictor@gmail.com>
2024-07-14 15:45:14 +0100

    Start working on client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Link from "next/link";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Home page</h1>
-      <p>
-        <Link href="/users">Users</Link>
-      </p>
-    </main>
-  );
+    return (
+      <main className={inter.className}>
+        
+      </main>
+    )
 }

5de96fb371a9bd257eb2ec4f6c8d44b5ae1093ac
Viktor Rura <ruravictor@gmail.com>
2024-07-14 17:05:23 +0100

    Add serach to navbar

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState, FormEvent } from "react"
 import { useRouter } from "next/navigation"
 
 
@@ -8,7 +8,7 @@ export default function Search() {
     const [search, setSearch] = useState("")
     const router = useRouter()
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setSearch("")
         router.push(`/${search}/`)
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Navbar from "./components/Navbar";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-slate-800">
         <Navbar />
+        
         {children}
 
       </body>

b58d7e365e3313a72437b0c4368587e188a2608e
Viktor Rura <ruravictor@gmail.com>
2024-07-14 12:00:44 +0100

    Add metadata to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'My Personal Blog - Thoughts on Technology, Travel, and Life',
+  description: 'Join me on my personal blog where I share my thoughts on the latest in technology, my travel adventures, and musings on life. Updated weekly!',
+}
 
 export default function Home() {
   return (
     <main className="min-h-full grid place-content-center bg-slate-500">
-      <h1 className="text-3xl text-red-700">Hello world</h1>
+      <h1 className="text-3xl text-indigo-800">Hello world</h1>
       <Link href="/about">
         Go to about page
       </Link>

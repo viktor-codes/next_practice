fa001f7e8cf7a4aa721d7719ebfc5acf9ba70e5b
Viktor Rura <ruravictor@gmail.com>
2024-07-15 18:43:35 +0100

    Add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
                 Hello and Welcome 👋&nbsp;
                 <span className="whitespace-nowrap">
-                    I&apos;m <span className="font-bold">Dave</span>.
+                    I&apos;m <span className="font-bold">Viktor</span>.
                 </span>
             </p>
         </main>

cd6556ebd307c922e7b82fee91e3f7e2d38e3634
Viktor Rura <ruravictor@gmail.com>
2024-07-15 18:55:10 +0100

    Add social icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from "react-icons/fa";
 
 export default function Navbar() {
   return (
@@ -12,7 +13,32 @@ export default function Navbar() {
                       Viktor Rura
                   </Link>
               </h1>
-              
+              <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+                  <Link
+                      className="text-white/90 hover:text-white"
+                      href="https://www.youtube.com/@DaveGrayTeachesCode"
+                  >
+                      <FaYoutube />
+                  </Link>
+                  <Link
+                      className="text-white/90 hover:text-white"
+                      href="https://courses.davegray.codes/"
+                  >
+                      <FaLaptop />
+                  </Link>
+                  <Link
+                      className="text-white/90 hover:text-white"
+                      href="https://github.com/gitdagray"
+                  >
+                      <FaGithub />
+                  </Link>
+                  <Link
+                      className="text-white/90 hover:text-white"
+                      href="https://twitter.com/yesdavidgray"
+                  >
+                      <FaTwitter />
+                  </Link>
+              </div>
           </div>
       </nav>
   );

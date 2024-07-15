db8dc9664be60f0c94e228763283cff133834b04
Viktor Rura <ruravictor@gmail.com>
2024-07-15 18:23:32 +0100

    Add error page

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,4 +1,5 @@
 import getWikiResults from "@/lib/getWikiResults"
+import Item from "./components/Item"
 
 type Props = {
     params: {
@@ -31,7 +32,7 @@ export default async function SearchResults({params: {searchTerm}}: Props) {
       <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
           {results 
               ? Object.values(results).map(result => {
-                  return <p>{JSON.stringify(result)}</p>;
+                  return <Item key={result.pageid} result={result} />
               })
            : (
               <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>

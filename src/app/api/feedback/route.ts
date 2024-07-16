a5fb9d72bb1540054b183b003fa14f2b181a0b22
Viktor Rura <ruravictor@gmail.com>
2024-07-16 11:06:50 +0100

    Add feedbak and thank-you pages

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -1,17 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server"
 
 type Feedback = {
-    name?: string;
-    email?: string;
-    feedback?: string;
-};
+    name?: string,
+    email?: string,
+    message?: string,
+}
 
+export async function POST(request: NextRequest) {
+    const data: Feedback = await request.json()
+    console.log('data: ', data)
 
-export async function POST(request: Request) {
-    const data: Feedback = await request.json();
-    console.log("data: ", data);
+    const { name, email, message } = data
 
-    const { name, email, feedback } = data;
-
-    return NextResponse.json({ name, email, feedback });
+    return NextResponse.json({ name, email, message })
 }

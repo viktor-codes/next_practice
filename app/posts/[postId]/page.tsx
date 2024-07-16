fc7d6191c5bce36027e8c046d0c28b1891b58726
Viktor Rura <ruravictor@gmail.com>
2024-07-16 09:42:19 +0100

    Make SSG

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -4,6 +4,16 @@ import { notFound } from "next/navigation"
 import Link from "next/link"
 
 
+export function generateStaticParams() {
+    const posts = getSortedPostsData()
+    return posts.map(post => ({
+        
+            postId: post.id,
+    
+    }));
+
+}
+
 export function generateMetadata({params}:{params: {postId: string}}) {
 
     const posts = getSortedPostsData()

c365effc0ba399352b700b6abe5d8d88cb36c1dd
Viktor Rura <ruravictor@gmail.com>
2024-07-16 09:37:54 +0100

    Add Post link to post page

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,5 +1,8 @@
-import { getSortedPostsData } from "@/lib/posts"
+import getFormattedDate from "@/lib/getFormattedDate"
+import { getSortedPostsData, getPostData } from "@/lib/posts"
 import { notFound } from "next/navigation"
+import Link from "next/link"
+
 
 export function generateMetadata({params}:{params: {postId: string}}) {
 
@@ -13,6 +16,7 @@ export function generateMetadata({params}:{params: {postId: string}}) {
             title: "Post not found",
         };
     }
+
     return {
         title: post.title,
     }; 
@@ -27,9 +31,20 @@ export default async function Post({params}:{params: {postId: string}}) {
         return notFound()
     }
 
+    const { title, date, contentHtml } = await getPostData(postId)
+
+    const pubDate = getFormattedDate(date)
+
   return (
-    <div>
-      
-    </div>
-  )
+      <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+          <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+          <p className="mt-0">{pubDate}</p>
+          <article>
+              <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+              <p>
+                  <Link href="/">← Back to home</Link>
+              </p>
+          </article>
+      </main>
+  );
 }

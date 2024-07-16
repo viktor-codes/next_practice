bac9101e5c06705b623d78cf21c1c649cadfd6c3
Viktor Rura <ruravictor@gmail.com>
2024-07-16 16:47:38 +0100

    Add middleware file

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -2,9 +2,15 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
-export async function GET(request: Request) {
+type Props = {
+    params: {
+        id: string
+    }
+}
 
-    const id = request.url.slice(request.url.lastIndexOf("/") + 1)
+export async function GET(request: Request, { params: { id } }: Props) {
+
+    // const id = request.url.slice(request.url.lastIndexOf("/") + 1)
 
     const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: Request) {
+
+    const regex = new RegExp("/api/*");
+
+    if (regex.test(request.url)){
+
+    }
+
+    console.log("Middleware called");
+    console.log(request.method)
+    console.log(request.url)
+
+    const origin = request.headers.get("origin");
+    console.log(origin)
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: "/api/:path*"
+}

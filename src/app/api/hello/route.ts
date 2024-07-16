86aed747a20061145a0619a056f1b6b8f6ce0a50
Viktor Rura <ruravictor@gmail.com>
2024-07-16 17:06:39 +0100

    Add token count to hello route

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,8 +1,25 @@
- 
+import {NextResponse} from "next/server"
 import { limiter } from "../config/limiter" 
 
 
-export async function GET() {
+export async function GET(request: Request) {
+
+    const origin = request.headers.get("origin");
+
     const remaining = await limiter.removeTokens(1)
+
+    console.log("remaining", remaining)
+    
+    if(remaining < 0) {
+        return new NextResponse(null, {
+            status: 429,
+            statusText: "Too Many Requests",
+            headers: {
+                "Access-Control-Allow-Origin": origin || "*",
+                "Content-Type": "text/plain"
+            }
+        })
+    }
+    
     return Response.json({"message": "Hello, Next.js!"})
 }

ce733320d9b111d0b49611032edfa1ea61c2298e
Viktor Rura <ruravictor@gmail.com>
2024-07-16 12:25:30 +0100

    Add POST method

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -12,6 +12,26 @@ export async function GET() {
     return NextResponse.json(todos);
 }
 
+export async function POST(request: Request) {
+    const {userId, title}: Partial<Todo> = await request.json();
+
+    if (!userId || !title) return NextResponse.json({"message": "Missing required data"});
+
+    const res = await fetch(`${DATA_SOURCE_URL}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "API-Key": API_KEY
+        },
+        body: JSON.stringify({userId, title, completed: false})
+    });
+
+    const newTodo: Todo = await res.json();
+
+    return NextResponse.json(newTodo);
+
+}
+
 export async function DELETE(request: Request) {
     const {id}: Partial<Todo> = await request.json();
 

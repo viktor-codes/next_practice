8b6bf8437ee27962b7e746818a27afef863eea02
Viktor Rura <ruravictor@gmail.com>
2024-07-16 12:30:29 +0100

    Add PUT method

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -32,6 +32,26 @@ export async function POST(request: Request) {
 
 }
 
+export async function PUT(request: Request) {
+    const {userId, id, title, completed}: Partial<Todo> = await request.json();
+
+    if (!userId || !id || typeof(completed) !== "boolean" || !title) return NextResponse.json({"message": "Missing required data"});
+
+    const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "API-Key": API_KEY
+        },
+        body: JSON.stringify({userId, title, completed})
+    });
+
+    const updatedTodo: Todo = await res.json();
+
+    return NextResponse.json(updatedTodo);
+
+}
+
 export async function DELETE(request: Request) {
     const {id}: Partial<Todo> = await request.json();
 

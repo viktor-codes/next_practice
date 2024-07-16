29412376972761286c28deb9a52e4873559b8245
Viktor Rura <ruravictor@gmail.com>
2024-07-16 17:15:26 +0100

    Add allowedOrigins to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,28 @@
 import { NextResponse } from "next/server";
 
+const allowedOrigins = process.env.NODE_ENV === "production" 
+    ? ["https://www.your-site.com", "https://your-site.com"] 
+    : ["http://localhost:3000", "https://www.google.com"]
+
 export function middleware(request: Request) {
+    const origin = request.headers.get("origin");
+    console.log(origin)
 
-    const regex = new RegExp("/api/*");
-
-    if (regex.test(request.url)){
-
+    if (origin && !allowedOrigins.includes(origin)) {
+        return new NextResponse(null, {
+            status: 400,
+            statusText: "Bad request",
+            headers: {
+                "Content-Type": "text/plain"
+            }
+        })
     }
 
     console.log("Middleware called");
     console.log(request.method)
     console.log(request.url)
 
-    const origin = request.headers.get("origin");
-    console.log(origin)
+    
 
     return NextResponse.next();
 }

7dbf9fd8135042347a64ca771be898190d2f8194
Viktor Rura <ruravictor@gmail.com>
2024-07-16 09:17:45 +0100

    Add dynamic posts

---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,19 @@
+import { getSortedPostsData } from "@/lib/posts"
+import { notFound } from "next/navigation"
+
+
+export default async function Post({params}:{params: {postId: string}}) {
+
+    const posts = getSortedPostsData()
+    const { postId } = params
+
+    if (!posts.find(post => post.id === postId)) {
+        return notFound()
+    }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
